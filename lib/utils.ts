0d48c1ef8cfb32f135f2a67fc3186fa71188e077
Jin <jinojoycomillas20@gmail.com>
2024-03-08 21:31:25 +0800

    chore: Add utility functions for later use

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,32 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function generateRandomString(length: number): string {
+	const charset =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * charset.length);
+		result += charset[randomIndex];
+	}
+
+	return result;
+}
+
+/**
+ *
+ * @param file The image file
+ * @returns A FormData obj with "file" and "filename" as keys
+ */
+export function imageWrapper(file: File) {
+	const formData = new FormData();
+	formData.append("file", file);
+	formData.append("filename", generateRandomString(10));
+
+	return formData;
 }
