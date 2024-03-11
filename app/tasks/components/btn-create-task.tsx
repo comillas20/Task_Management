17ae4596eaded9d8a6e46b73ddd57270d2e9e713
Jin <jinojoycomillas20@gmail.com>
2024-03-11 19:51:20 +0800

    refactor: Change "new" to "create"

---
+++ app/tasks/components/btn-create-task.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { PlusIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { TaskDialog } from "./task-dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+
+export function CreateTask() {
+  return (
+    <TaskDialog>
+      <DialogTrigger asChild>
+        <Button size="sm" className="ml-auto h-8 shadow-md lg:flex">
+          <PlusIcon className="mr-2 h-4 w-4" />
+          Task
+        </Button>
+      </DialogTrigger>
+    </TaskDialog>
+  );
+}
